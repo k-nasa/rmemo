85dc2e7ca6dff76d2476f7ea704fb2690966ba79
k-nasa <htilcs1115@gmail.com>
2018-09-24 22:16:18 +0900

    add/ Create a config file

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,36 @@
+use std::fs::*;
+use std::io::Result;
+use std::path::*;
+
+extern crate dirs;
+
 fn main() {
-    println!("Hello, world!");
+    let config = match Config::load_config() {
+        Ok(config) => config,
+        Err(e) => {
+            println!("{}", e);
+            return;
+        }
+    };
+}
+
+pub struct Config;
+
+/// Read the file in which the setting file is described.
+/// If not, create it
+impl Config {
+    fn load_config() -> Result<Config> {
+        //FIXME Not compatible with windows
+        let dir = match dirs::home_dir() {
+            Some(dir) => Path::new(&dir.to_str().unwrap().to_string()).join(".config/memo/"),
+            _ => Path::new("./").join(".config/memo/"),
+        };
+
+        let filepath = &dir.join("config.toml");
+
+        // will create it every time, but it may be easy
+        let file = File::create(filepath);
+
+        Ok(Config)
+    }
 }
