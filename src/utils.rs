819f1df52cb579417dfe87f3aa256fcdc4a213f1
k-nasa <htilcs1115@gmail.com>
2018-09-28 15:32:44 +0900

    Add utils module

---
+++ src/utils.rs
@@ -0,0 +1,16 @@
+use dirs::home_dir;
+use std::io::*;
+use std::str::FromStr;
+
+pub fn read<T: FromStr>() -> T {
+    let mut s = String::new();
+    stdin().read_line(&mut s).ok();
+    s.trim().parse().ok().unwrap()
+}
+
+pub fn home_dir_string() -> String {
+    match home_dir() {
+        Some(dir) => dir.to_str().unwrap().to_string(),
+        _ => panic!("Home directory is not set"),
+    }
+}
