2d0866a0c17905ad096ad40bf10839568f1db40a
k-nasa <htilcs1115@gmail.com>
2018-09-28 15:52:33 +0900

    delete/ unused import

--- src/commands.rs
+++ src/commands.rs
@@ -1,8 +1,6 @@
 use chrono::prelude::*;
 use clap::{App, AppSettings, Arg, ArgMatches, SubCommand};
 use std::fs::*;
-use std::io::prelude::*;
-use std::io::Result;
 use std::path::*;
 use std::process::{Command, Stdio};
 use std::str::from_utf8;
