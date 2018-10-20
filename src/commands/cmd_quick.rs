138e5667ea140361bc5e7c2955ab24635b3260bf
k-nasa <htilcs1115@gmail.com>
2018-10-20 15:38:22 +0900

    Add feature append idea to end of storage file

--- src/commands/cmd_quick.rs
+++ src/commands/cmd_quick.rs
@@ -1,6 +1,9 @@
 use super::run_editor;
 use clap::ArgMatches;
+use colored::*;
 use config::Config;
+use std::fs::OpenOptions;
+use std::io::prelude::*;
 
 pub fn cmd_quick(matches: &ArgMatches, config: &Config) {
     match matches.value_of("your idea") {
@@ -9,7 +12,27 @@ pub fn cmd_quick(matches: &ArgMatches, config: &Config) {
     }
 }
 
-fn append_idea_to_file(idea: &str, config: &Config) {}
+fn append_idea_to_file(idea: &str, config: &Config) {
+    let dir = config.memos_dir();
+    let filepath = format!("{}/storage_place_of_your_idea.md", dir);
+
+    let mut file = match OpenOptions::new()
+        .create(true)
+        .write(true)
+        .append(true)
+        .read(true)
+        .open(filepath)
+    {
+        Ok(file) => file,
+        Err(e) => panic!(e),
+    };
+
+    let idea = format!("{}\n", idea);
+    match file.write_all(idea.as_bytes()) {
+        Ok(_) => println!("{}", "Suceess".green()),
+        Err(e) => println!("{}", e),
+    }
+}
 
 fn open_idea_file_with_editor(config: &Config) {
     let dir = config.memos_dir();
