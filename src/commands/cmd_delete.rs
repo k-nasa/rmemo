4c1fe8b191bce45b4f451828e9ec553fd06c74e3
k-nasa <htilcs1115@gmail.com>
2018-10-21 21:38:49 +0900

    Add DirOrFile struct

--- src/commands/cmd_delete.rs
+++ src/commands/cmd_delete.rs
@@ -19,6 +19,12 @@ macro_rules! confirmation {
     };
 }
 
+struct FileOrDir {
+    name: String,
+    path: String,
+    is_dir: bool,
+}
+
 pub fn cmd_delete(matches: &ArgMatches, config: &Config) {
     let pattern = match matches.value_of("pattern") {
         Some(pattern) => pattern.to_string(),
