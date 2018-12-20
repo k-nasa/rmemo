1281fa5d483bcee95d514b692dd90278d4c246a0
k-nasa <htilcs1115@gmail.com>
2018-12-20 20:07:48 +0900

    Delete comment

--- src/commands/cmd_list.rs
+++ src/commands/cmd_list.rs
@@ -1,14 +1,8 @@
-use clap::ArgMatches;
 use crate::config::Config;
 use crate::dir_tree::*;
+use clap::ArgMatches;
 
 pub fn cmd_list(matches: &ArgMatches, config: &Config) {
-    // TODO Implementation of filtering by patter
-    // let pattern = match matches.value_of("pattern") {
-    //     Some(pattern) => pattern.to_string(),
-    //     None => String::new(),
-    // };
-
     let memo_dir = config.memos_dir();
     let dir_tree = DirTree::new(memo_dir);
 
