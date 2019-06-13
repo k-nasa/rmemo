959659ef82c4e5cf950eb4e154e0ede2243a6ed7
k-nasa <htilcs1115@gmail.com>
2019-06-14 08:40:39 +0900

    remove to_snakecase

--- src/commands/new.rs
+++ src/commands/new.rs
@@ -9,11 +9,11 @@ use std::string::*;
 
 pub fn cmd_new(matches: &ArgMatches, config: &Config) {
     let input_filepath = match matches.value_of("title") {
-        Some(title) => to_snake_case(&title.to_string()),
+        Some(title) => title.to_string(),
         None => {
             print!("Input title :");
             std::io::stdout().flush().expect("print! is faild");
-            to_snake_case(&crate::utils::read::<String>())
+            crate::utils::read::<String>()
         }
     };
 
